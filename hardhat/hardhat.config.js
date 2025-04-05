99c875d5c774422e5100e64e47a1b05faa14ae87
Xiangyu Wong <wongxiangyu22@gmail.com>
2025-04-06 06:33:34 +0800

    Added subscription nft contract

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,45 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    bscTestnet: {
+      url: "https://bsc-testnet.infura.io/v3/" + process.env.INFURA_API_KEY,
+      chainId: 97,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
+    garfield: {
+      url: "https://garfield-testnet.zircuit.com/",
+      chainId: 48898,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    },
+    baseSepolia: {
+      url: "https://base-sepolia.infura.io/v3/" + process.env.INFURA_API_KEY,
+      chainId: 84532,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: process.env.BSCSCAN_API_KEY || "",
+    customChains: [
+      {
+        network: "bscTestnet",
+        chainId: 97,
+        urls: {
+          apiURL: "https://api-testnet.bscscan.com/api",
+          browserURL: "https://testnet.bscscan.com"
+        }
+      }
+    ]
+  }
+};
