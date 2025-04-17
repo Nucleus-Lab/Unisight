6e05cf37a44bdaf1a38c69e768f6c01185307f10
Xiangyu Wong <wongxiangyu22@gmail.com>
2025-04-17 12:44:04 +0800

    feat: added config for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/unisight/',
+  server: {
+    host: '0.0.0.0',  // Allow listening on all network interfaces
+    port: 5173,        // The port Vite should run on
+    allowedHosts: [
+      'nucleuslab.xyz',  // Allow requests from your domain
+      'localhost',        // Allow local development
+      '127.0.0.1',        // Allow local IP
+    ],
+  },
 })
